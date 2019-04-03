74afc890dad5505bbe2017f90b3526d5bb2f8a09
77web <h-hishida@quartetcom.co.jp>
2019-04-03 14:25:38 +0900

    令和を追加

---
+++ tests/Wareki/ReiwaTest.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Quartetcom\TryDependencyAnalyzer\Wareki;
+
+
+use PHPUnit\Framework\TestCase;
+
+class ReiwaTest extends TestCase
+{
+    public function test_supports()
+    {
+        $SUT = $this->getSUT();
+        $this->assertFalse($SUT->supports(new \DateTimeImmutable('2019-04-30')));
+        $this->assertTrue($SUT->supports(new \DateTimeImmutable('2019-05-01')));
+    }
+
+    public function test_provide()
+    {
+        $SUT = $this->getSUT();
+
+        $this->assertEquals('令和元年5月1日', $SUT->provide(new \DateTimeImmutable('2019-05-01')));
+        $this->assertEquals('令和2年1月1日', $SUT->provide(new \DateTimeImmutable('2020-01-01')));
+    }
+
+    private function getSUT()
+    {
+        return new Reiwa();
+    }
+}
